bdee374ba58a2ba8216ee5aef1837f5dda1e4715
Michał Budzyński <michal@virtualdesign.pl>
2015-11-08 01:01:00 +0100

    :waning_gibbous_moon: main require config for the app

---
+++ src/app/require.config.js
@@ -0,0 +1,10 @@
+// require.js looks for the following global when initializing
+var require = {
+  baseUrl: ".",
+  paths: {
+      "knockout": "bower_modules/knockout/dist/knockout",
+      "text":     "bower_modules/requirejs-text/text"
+  },
+  
+  urlArgs: "bust=" + (new Date()).getTime()
+};
